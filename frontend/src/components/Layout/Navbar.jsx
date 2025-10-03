02a4a1de2ade19ae20e9afd6075e52eeb86f7d44
nis chal <khanaln07@gmail.com>
2025-10-03 12:00:45 +0545

    chalyo

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -32,7 +32,7 @@ export default function Navbar() {
   },
       });
         const data = await res.json();
-        setCart(data.items);
+        setCart(data);
         console.log("d",data)
       } catch (err) {
         console.error("Failed to fetch uploads", err);
